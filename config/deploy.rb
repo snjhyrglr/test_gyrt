c27cdd98e14b7aa67c6ef91c2a9b3fa665c58df7
snjhyrglr <jayjregalario@gmail.com>
2023-01-30 15:07:08 +0800

    update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :repo_url, "git@github.com:snjhyrglr/test_gyrt.git"
 set :branch, "main"
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
-set :deploy_to, "/var/www/#{fetch :application}"
+set :deploy_to, "/var/www/#{fetch(:application)}"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
