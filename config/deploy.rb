d2f6313e3481241c1ffa23343acbaa502a15bc37
snjhyrglr <jayjregalario@gmail.com>
2023-01-30 14:22:02 +0800

    add(again) master.key

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,6 +30,19 @@ set :deploy_to, "/var/www/#{fetch :application}"
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+append :linked_files, "config/master.key"
+
+namespace :deploy do
+  namespace :check do
+    before :linked_files, :set_master_key do
+      on roles(:app), in: :sequence, wait: 10 do
+        unless test("[ -f #{shared_path}/config/master.key ]")
+          upload! 'config/master.key', "#{shared_path}/config/master.key"
+        end
+      end
+    end
+  end
+end
 
 
 # Default value for default_env is {}
