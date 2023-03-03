f2a72d36ad9bb30c06bea95ef2c986b107792957
snjhyrglr <jayjregalario@gmail.com>
2023-03-03 15:59:26 +0800

    add dept to employee

--- db/migrate/20230303022235_add_department_to_employee.rb
+++ db/migrate/20230303022235_add_department_to_employee.rb
@@ -1,5 +1,5 @@
 class AddDepartmentToEmployee < ActiveRecord::Migration[7.0]
   def change
-    add_reference :employees, :department, null: false, foreign_key: true
+    add_reference :department, :employee, null: false, foreign_key: true
   end
 end
